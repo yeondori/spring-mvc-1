188a7a4b9d6d47b5bf3da9ae0249554d13f202a4
yeondori <yeonseo.jung0@gmail.com>
2024-02-05 19:55:52 +0900

    feat: 요청, 응답 받기[섹션 2]

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override // Servlet이 호출되면 service가 실행됨
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        super.service(req, resp);
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+
+}
